c1d046afb9f762bda64dd05c7470e8f1d0e95fd7
williamwhang <whang.william@gmail.com>
2021-11-14 10:05:34 -0300

    Finalizado

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
+import { ToastContainer } from 'react-toastify';
+import GlobalStyle from '../styles/GlobalStyles';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ToastContainer />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
